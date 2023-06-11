fc6bb6f4a759c1d3503e47a20f5798b0c1f82e04
Yaswanth <yxpanguluri@shockers.wichita.edu>
2023-06-10 20:39:45 -0500

    added makeSound function

--- Drum_Kit/index.js
+++ Drum_Kit/index.js
@@ -2,8 +2,25 @@ var noOfDrums = document.querySelectorAll(".drum").length;
 
 document.addEventListener("keypress",(event)=>
 {
-    //console.log(event.key);
-    let selected = event.key
+    
+    makeSound(event.key);
+}
+);
+
+for(let i=0;i<=noOfDrums;i++)
+{
+   document.querySelectorAll(".drum")[i].addEventListener
+   (
+    "click",
+        function () 
+        {
+            makeSound(this.innerHTML)
+        }
+    ); 
+};
+
+
+function makeSound (selected) {
     switch(selected)
             {
                 case 'w':
@@ -45,58 +62,3 @@ document.addEventListener("keypress",(event)=>
 
             }
 }
-);
-
-for(let i=0;i<=noOfDrums;i++)
-{
-   document.querySelectorAll(".drum")[i].addEventListener
-   (
-    "click",
-        function () 
-        {
-            let selected = this.innerHTML;
-            switch(selected)
-            {
-                case 'w':
-                    var audio = new Audio("sounds/tom-1.mp3");
-                    audio.play();
-                    break;
-
-                case 'a':
-                    var audio = new Audio("sounds/tom-2.mp3");
-                    audio.play();
-                    break;
-                
-                case 's':
-                    var audio = new Audio("sounds/tom-3.mp3");
-                    audio.play();
-                    break;
-
-                case 'd':
-                    var audio = new Audio("sounds/tom-4.mp3");
-                    audio.play();
-                    break;
-
-                case 'j':
-                    var audio = new Audio("sounds/snare.mp3");
-                    audio.play();
-                    break;
-
-                case 'k':
-                    var audio = new Audio("sounds/crash.mp3");
-                    audio.play();
-                    break;
-
-                case 'l':
-                    var audio = new Audio("sounds/kick-bass.mp3");
-                    audio.play();
-                    break;
-                default:
-                    console.log("Invalid");
-
-            }
-        }
-    ); 
-};
-
-
