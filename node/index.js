b1384e048e089f1b229744506ae440bdeac28860
Yaswanth <yxpanguluri@shockers.wichita.edu>
2023-06-17 17:49:11 -0500

    Node Js

---
+++ node/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+fs.writeFile("message.txt", "Hello yaswanth", (error) => {
+  if (error) throw error;
+  console.log("The file have been saved!");
+});
+
+fs.readFile("message.txt",'utf8',(err,data)=>{
+    if(err) throw err;
+    console.log(data);
+});
